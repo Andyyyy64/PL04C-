5614f3921659485678070ce098dbad88ded5fc49
Andyyyy64 <takuyarossi77@gmail.com>
2024-10-03 12:34:20 +0900

    init

---
+++ ex01/name.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string fn, ln;
+
+    cout << "Enter your family name: ";
+    cin >> ln;
+
+    cout << "Enter your first name: ";
+    cin >> fn;
+
+    cout << "hello, " << fn << " " << ln << endl;
+    return 0;
+}
